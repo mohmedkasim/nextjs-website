2c31146eb37ee84191a1cc76faa4c09b7ece22d4
mohmedkasim <56302970+mohmedkasim@users.noreply.github.com>
2023-01-19 09:58:47 -0600

    add section

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { BsFillMoonStarsFill } from "react-icons/bs";
 
 export default function Home() {
   return (
@@ -9,9 +10,24 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <section className="bg-blue-700 ">
-          <h1>Hello</h1>
+      <main className="bg-white px-10">
+        <section className="min-h-screen">
+          <nav className="py-10 mb-12 flex justify-between">
+            <h1 className="text-xl transition duration-300 hover:scale-110 font-burtons">Medo</h1>
+            <ul className="flex items-center">
+              <li>
+                <BsFillMoonStarsFill className="cursor-pointer text-2xl" />
+              </li>
+              <li className="transition duration-300 hover:scale-110">
+                <a
+                  href="#"
+                  className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8 "
+                >
+                  Resume
+                </a>
+              </li>
+            </ul>
+          </nav>
         </section>
       </main>
     </>
