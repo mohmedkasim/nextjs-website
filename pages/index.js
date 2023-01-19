72197716e4e4fc19f5c28b8959cc07dd0e8ef160
mohmedkasim <56302970+mohmedkasim@users.noreply.github.com>
2023-01-19 10:31:51 -0600

    add changes

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 import Head from "next/head";
 import { BsFillMoonStarsFill } from "react-icons/bs";
+import {
+  AiFillTwitterCircle,
+  AiFillLinkedin,
+  AiFillYoutube,
+} from "react-icons/ai";
 
 export default function Home() {
   return (
@@ -13,7 +18,9 @@ export default function Home() {
       <main className="bg-white px-10">
         <section className="min-h-screen">
           <nav className="py-10 mb-12 flex justify-between">
-            <h1 className="text-xl transition duration-300 hover:scale-110 font-burtons">Medo</h1>
+            <h1 className="text-xl transition duration-300 hover:scale-110 font-burtons">
+              Medo
+            </h1>
             <ul className="flex items-center">
               <li>
                 <BsFillMoonStarsFill className="cursor-pointer text-2xl" />
@@ -28,6 +35,22 @@ export default function Home() {
               </li>
             </ul>
           </nav>
+          <div className="text-center p-10">
+            <h2 className="text-5xl py-2 text-teal-600 font-medium">
+              Mohamed Kasim
+            </h2>
+            <h3 className="text-2xl py-2">Developer and Product Desinger</h3>
+            <p className="text-md py-5 leading-8 text-gray-800">
+              Freelancer providiner services for programming and product design.
+              Check me down below my previous work
+            </p>
+          </div>
+          <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600">
+            <AiFillTwitterCircle />
+            <AiFillYoutube />
+            <AiFillLinkedin />
+          </div>
+          <div className=""></div>
         </section>
       </main>
     </>
